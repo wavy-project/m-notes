3e11c3f4f284afc6c532e82fb6ca0509b147a0ef
Mateo Garcia <mateog@stanford.edu>
2019-01-20 14:01:39 -0800

    Initial Commit

---
+++ m-notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m-notes
+//
+//  Created by Mateo Garcia on 1/20/19.
+//  Copyright © 2019 wavy-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
